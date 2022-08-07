a62ce5f3ebc45111ae95efbd168fc9ec25a7f92a
Frank14b <franckfontcha@gmail.com>
2022-08-07 12:04:24 +0400

    update for style

--- components/template/HeaderComponent.jsx
+++ components/template/HeaderComponent.jsx
@@ -10,7 +10,7 @@ export function HeaderComponent({ activeMenu = "", title = "Fils online general 
                 <link href="../../images/logo_icon.png" rel="icon" />
                 {/* <!-- required meta --> */}
                 <meta charset="UTF-8" />
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                {/* <meta http-equiv="X-UA-Compatible" content="IE=edge" /> */}
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 {/* <!-- keywords for this document --> */}
                 <meta name="keywords" content="FilsShop Filsonline, Trading, UAE, E-commerce, E-shop, Market, Online Market, Shop" />
